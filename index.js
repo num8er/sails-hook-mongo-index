bde044adb40e34946cea2d9241bd02126379f98b
Anar K. Jafarov <anar.k.jafarov@gmail.com>
2020-02-05 13:45:42 +0200

    fixes, verbose modes and etc.

--- index.js
+++ index.js
@@ -6,18 +6,25 @@ module.exports = function mongoIndexHook(sails) {
     const indexes = [];
     for(const modelName in sails.models) {
       const model = sails.models[modelName];
-      if(!sails.config.connections[model.connection]) continue;
-      if(sails.config.connections[model.connection].adapter !== 'sails-mongo') continue;
-    
+      if (!model) {
+        console.log('Model:', modelName, 'not initialized');
+        continue;
+      }
+      
+      const connectionName = Array.isArray(model.connection) ? model.connection[0] : model.connection;
+      if(!sails.config.connections[connectionName]) continue;
+      if(sails.config.connections[connectionName].adapter !== 'sails-mongo') continue;
+      
+      
       for(const attributeName in model.attributes) {
         const attribute = model.attributes[attributeName];
         if(attribute.primaryKey) continue;
         if(attribute.unique === true) {
-          indexes.push({model, attributeName, unique: true});
+          indexes.push({modelName, model, attributeName, unique: true});
           continue;
         }
         if(attribute.index === true) {
-          indexes.push({model, attributeName, unique: false});
+          indexes.push({modelName, model, attributeName, unique: false});
         }
       }
     }
@@ -38,10 +45,16 @@ module.exports = function mongoIndexHook(sails) {
   
   function ensureIndex(index) {
     return new Promise((resolve, reject) => {
-      const {model, attributeName, unique} = index;
-  
+      const {modelName, model, attributeName, unique} = index;
+      
       model.native(function (error, collection) {
         if (error) return reject(error);
+        
+        if (process.env.NODE_ENV !== 'production'
+          || process.env.DEBUG === 'true'
+          || process.env.VERBOSE === 'true') {
+          console.log('Ensuring index for:', modelName+'.'+attributeName);
+        }
         
         collection.ensureIndex(
           attributeName,
@@ -57,6 +70,11 @@ module.exports = function mongoIndexHook(sails) {
   let hookCalled = false;
   
   return {
+    defaults: {
+      __configKey__: {
+        _hookTimeout: 30000
+      }
+    },
     initialize: function (cb) {
       sails.on('hook:orm:loaded', async function () {
         if (!hookCalled) hookCalled = true;
